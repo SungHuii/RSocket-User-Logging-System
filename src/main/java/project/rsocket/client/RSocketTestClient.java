1f6ada74d2507cb77dcd1fbb50d8264d9f7da305
홍성휘 <gkemg2017@gmail.com>
2025-04-08 20:51:09 +0900

    #2 간단한 RSocket 구조 작성
    RSocketTestClient와 ActivityLogController 수정

--- src/main/java/project/rsocket/client/RSocketTestClient.java
+++ src/main/java/project/rsocket/client/RSocketTestClient.java
@@ -1,4 +1,27 @@
 package project.rsocket.client;
 
-public class RSocketTestClient {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class RSocketTestClient implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        RSocketRequester requester = RSocketRequester.builder()
+                .dataMimeType(MediaType.APPLICATION_JSON)
+                .tcp("localhost", 7000);
+
+        requester.route("log.activity")
+                .data("User clicked on 'Join Now' button")
+                .send()
+                .subscribe();
+
+        log.info("Log sent via Fire-and-Forget");
+    }
 }
--- src/main/java/project/rsocket/controller/ActivityLogController.java
+++ src/main/java/project/rsocket/controller/ActivityLogController.java
@@ -1,4 +1,21 @@
 package project.rsocket.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
 public class ActivityLogController {
+
+    @MessageMapping("log.activity")
+    public Mono<Void> logUserActivity(String message) {
+
+        // 실제 구현 시 DB 저장 등 처리
+        log.info("Received User activity logged: {}", message);
+        return Mono.empty(); // fire-and-forget 은 응답 없음
+    }
 }
