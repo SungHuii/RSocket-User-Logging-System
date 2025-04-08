647ae0ef291afc357eca8978efbbb5f75971d6b1
홍성휘 <gkemg2017@gmail.com>
2025-04-08 20:53:56 +0900

    #3 DTO 추가
    단순 문자열 출력에서 DTO 전송으로 변환하기 위한 DTO 추가

--- src/main/java/project/rsocket/dto/LogMessageDTO.java
+++ src/main/java/project/rsocket/dto/LogMessageDTO.java
@@ -1,4 +1,14 @@
 package project.rsocket.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LogMessageDTO {
+    private String userId;
+    private String action;
+    private String timestamp;
 }
